f646947697ccc7930d1e771558ab61d0796e62d5
ram-tester <rampatropj@gmail.com>
2025-05-16 10:48:11 +0530

    cluster config added in eks.yaml

--- docker.tf
+++ docker.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "this" {
   ami = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
   instance_type = "t3.micro"
-  # root_block_device {
-  #   volume_size = 50
-  #   volume_type = "gp3"
-  # }
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+  }
   user_data = file("docker.sh")
   tags = {
     Name = "docker"
